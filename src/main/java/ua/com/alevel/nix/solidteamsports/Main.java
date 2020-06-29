9a5154fef5a158e3f8c5c84aa6059dee29ba9dc0
ArturPupchenko <pupchenkoartur@gmail.com>
2020-06-29 19:15:54 +0300

    Main

--- src/main/java/ua/com/alevel/nix/solidteamsports/Main.java
+++ src/main/java/ua/com/alevel/nix/solidteamsports/Main.java
@@ -2,12 +2,13 @@ package ua.com.alevel.nix.solidteamsports;
 
 import ua.com.alevel.nix.solidteamsports.data.Audience;
 import ua.com.alevel.nix.solidteamsports.data.Sportsmen;
+import ua.com.alevel.nix.solidteamsports.service.SportsmenService;
 import ua.com.alevel.nix.solidteamsports.service.impl.SportsmenServiceImpl;
 
 public class Main {
 
     public static void main(String[] args) {
-        SportsmenServiceImpl sportsmenServiceImpl = new SportsmenServiceImpl();
-        sportsmenServiceImpl.startGame(new Sportsmen(), new Audience());
+        SportsmenService sportsmenService = new SportsmenServiceImpl();
+        sportsmenService.startGame(new Sportsmen(), new Audience());
     }
 }
